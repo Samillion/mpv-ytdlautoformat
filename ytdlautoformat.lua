981873e09f596c9b00f90242133bf47d79cdd377
Sam <17427046+Samillion@users.noreply.github.com>
2019-10-30 15:33:29 +0200

    Added support for multi-prupose playlists and some cleanup.
    This update adds support for multi-purpose playlists. For example, if you have a playlist that includes Youtube URLs, Vimeo URLs and local files on your HDD.
    
    Instead of changing `ytdl-format` once for all the playlist, now the script will do a one by one check, for more accuracy.
    
    In the example mentioned above, the `ytdl-format` will only change for Youtube, not for Vimeo and script will not run if it's a local file, since it's not a URL stream.
    
    The `ytdlLow` value will be used for matched domains in VSTREAM, otherwise `ytdlBest` will be used. So in essence, `ytdlBest` acts as the default `ytdl-format` you would normally set in `mpv.conf`

--- ytdlautoformat.lua
+++ ytdlautoformat.lua
@@ -1,17 +1,26 @@
 --[[
 
-A simple mpv script to automatically change ytdl-format (youtube-dl)
+A simple mpv script to automatically change ytdl-format (for youtube-dl)
 specifically if the URL is Youtube or Twitch.
 
 If the URL is Youtube or Twitch, ytdl-format is set to:
 480p, 30 FPS and no VP9.
 
+Otherwise, ytdl-format is set to:
+Best video quality, 30 FPS and no VP9
+
 To add more domains, simply add them to the VSTREAMS set.
 
-To adjust quality, simply change 480 to 720 for 720p, or 1080
-within the ytdlAutoChange() function.
+To adjust quality or FPS for either low quality or best/default quality
+change the values of ytdlLow and ytdlBest
+
+For example, ytdlLow will be used if URL is Youtube or Twitch, otherwise
+ytdlBest will be used.
 
 --]]
+
+ytdlLow = "bestvideo[height<=?480][fps<=?30][vcodec!=?vp9]+bestaudio/best[height<=480]"
+ytdlBest = "bestvideo[fps<=?30][vcodec!=?vp9]+bestaudio/best"
 
 local msg = require 'mp.msg'
 local utils = require 'mp.utils'
@@ -33,21 +42,34 @@ function get_streamsource(path)
 end
 
 
-function ytdlAutoChange()
-	local path = mp.get_property("path", "")
+function ytdlAutoChange(name, value)
+	local path = value
 
-	if string.match(string.lower(path), "^(https?://)") then
 		if VSTREAMS[string.lower(get_streamsource(path))] then
-			mp.set_property("ytdl-format", "bestvideo[height<=?480][fps<=?30][vcodec!=?vp9]+bestaudio/best[height<=480]")
-			msg.info("Match found, ytdl-format has been changed.")
-			msg.info("ytdl-format: "..mp.get_property("ytdl-format"))
+			mp.set_property("ytdl-format", ytdlLow)
+			msg.info("Domain match found, ytdl-format has been changed.")
+			msg.info("Changed ytdl-format: "..mp.get_property("ytdl-format"))
 		else
-			msg.info("No match, ytdl-format unchanged.")
+			msg.info("No domain match, ytdl-format unchanged.")
 		end
+
+	
+	mp.unobserve_property(ytdlAutoChange)
+	msg.info("Finished check, no longer observing ytdlAutoChange.")
+end
+
+function ytdlCheck()
+	local path = mp.get_property("path", "")
+	
+	if string.match(string.lower(path), "^(https?://)") then
+		mp.set_property("ytdl-format", ytdlBest)
+		msg.info("Current ytdl-format: "..mp.get_property("ytdl-format"))
 		
+		mp.observe_property("path", "string", ytdlAutoChange)
+		msg.info("Observing path to determine ytdlAutoChange status...")
 	else
 		msg.info("Not a URL/Stream. Script did not run.")
 	end
 end
 
-mp.register_event("start-file", ytdlAutoChange)
+mp.register_event("start-file", ytdlCheck)
