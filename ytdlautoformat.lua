c5ab779045257357b04f8e59c7792d68d6d5fa02
Sam <17427046+Samillion@users.noreply.github.com>
2019-10-30 19:40:29 +0200

    Improve domain matching and rename some variables, sets and functions.

--- ytdlautoformat.lua
+++ ytdlautoformat.lua
@@ -9,18 +9,18 @@ If the URL is Youtube or Twitch, ytdl-format is set to:
 Otherwise, ytdl-format is set to:
 Best video quality, 30 FPS and no VP9
 
-To add more domains, simply add them to the VSTREAMS set.
+To add more domains, simply add them to the StreamSource set.
 
 To adjust quality or FPS for either low quality or best/default quality
-change the values of ytdlLow and ytdlBest
+change the values of ytdlChange and ytdlDefault
 
-For example, ytdlLow will be used if URL is Youtube or Twitch, otherwise
-ytdlBest will be used.
+For example, ytdlChange will be used if URL is Youtube or Twitch, otherwise
+ytdlDefault will be used.
 
 --]]
 
-local ytdlLow = "bestvideo[height<=?480][fps<=?30][vcodec!=?vp9]+bestaudio/best[height<=480]"
-local ytdlBest = "bestvideo[fps<=?30][vcodec!=?vp9]+bestaudio/best"
+local ytdlDefault = "bestvideo[fps<=?30][vcodec!=?vp9]+bestaudio/best"
+local ytdlChange = "bestvideo[height<=?480][fps<=?30][vcodec!=?vp9]+bestaudio/best[height<=480]"
 
 local msg = require 'mp.msg'
 local utils = require 'mp.utils'
@@ -31,22 +31,22 @@ local function Set (t)
 	return set
 end
 
-local VSTREAMS = Set {
+local StreamSource = Set {
 	'youtu.be', 'youtube.com', 'www.youtube.com', 
 	'twitch.tv', 'www.twitch.tv'
 }
 
-local function get_streamsource(path)
+local function getStreamSource(path)
 	local hostname = path:match '^https?://([^/]+)/' or ''
-	return hostname:match '(%w+%.%w+)$'
+	return hostname:match '([%w%.]+%w+)$'
 end
 
 
 local function ytdlAutoChange(name, value)
 	local path = value
 
-	if VSTREAMS[string.lower(get_streamsource(path))] then
-		mp.set_property("ytdl-format", ytdlLow)
+	if StreamSource[getStreamSource(string.lower(path))] then
+		mp.set_property("ytdl-format", ytdlChange)
 		msg.info("Domain match found, ytdl-format has been changed.")
 		msg.info("Changed ytdl-format: "..mp.get_property("ytdl-format"))
 	else
@@ -55,20 +55,20 @@ local function ytdlAutoChange(name, value)
 
 	
 	mp.unobserve_property(ytdlAutoChange)
-	msg.info("Finished check, no longer observing ytdlAutoChange.")
+	msg.info("Finished check, script no longer running.")
 end
 
 local function ytdlCheck()
 	local path = mp.get_property("path", "")
 	
 	if string.match(string.lower(path), "^(https?://)") then
-		mp.set_property("ytdl-format", ytdlBest)
+		mp.set_property("ytdl-format", ytdlDefault)
 		msg.info("Current ytdl-format: "..mp.get_property("ytdl-format"))
 		
 		mp.observe_property("path", "string", ytdlAutoChange)
 		msg.info("Observing path to determine ytdlAutoChange status...")
 	else
-		msg.info("Not a URL/Stream. Script did not run.")
+		msg.info("Not a URL/Stream, script did not run.")
 	end
 end
 
