a004aeeb54b0e635ba01a7e8cabf4b7f4b43e714
Sam <17427046+Samillion@users.noreply.github.com>
2024-10-22 01:24:50 +0300

    Add more codec options

--- ytdlautoformat.lua
+++ ytdlautoformat.lua
@@ -1,26 +1,26 @@
 --[[
 
     A simple mpv script to automatically change ytdl-format (yt-dlp)
-    specifically if the URL is Youtube or Twitch, by default.
+    for specified domains/streams.
 
-    For more details:
-    https://github.com/Samillion/mpv-ytdlautoformat
+    Info: https://github.com/Samillion/mpv-ytdlautoformat
 
 --]]
 
 local options = {
-    -- Set video quality for ytdl-format
-    -- Accepts: 240, 360, 480, 720, 1080, 1440, 2160, 4320
-    quality = 720,
-
     -- Which domains should ytdl-format change on?
     domains = {
         "youtu.be", "youtube.com", "www.youtube.com", 
         "twitch.tv", "www.twitch.tv"
     },
 
-    -- Should Google's VP9 codec be used if found?
-    enableVP9 = false
+    -- Set video quality for auto ytdl-format (on load/start)
+    -- 240, 360, 480, 720, 1080, 1440, 2160, 4320
+    quality = 720,
+
+    -- Prefered codec. avc, vp9 or novp9
+    -- novp9: accept any codec except vp9
+    codec = "avc"
 }
 
 -- Do not edit beyond this point
@@ -32,10 +32,20 @@ local function Set(t)
     return set
 end
 
+local function update_ytdl_format()
+    local codec_list = {
+        ["avc"] = "[vcodec^='avc']",
+        ["vp9"] = "[vcodec^='vp0?9']",
+        ["novp9"] = "[vcodec!~='vp0?9']"
+    }
+    local codec = codec_list[options.codec:lower()] or ""
+    local ytdlCustom = "bv[height<=?" .. options.quality .. "]" .. codec .. "+ba/b[height<=" .. options.quality .. "]"
+    mp.set_property("file-local-options/ytdl-format", ytdlCustom)
+    mp.msg.info("Changed ytdl-format to: " .. ytdlCustom)
+end
+
 local msg = require "mp.msg"
 local list = Set(options.domains)
-local VP9 = options.enableVP9 and "" or "[vcodec!~='vp0?9']"
-local ytdlCustom = "bv[height<=?" .. options.quality .. "]" .. VP9 .. "+ba/b[height<=" .. options.quality .. "]"
 
 mp.add_hook("on_load", 9, function()
     local path = mp.get_property("path", "")
@@ -46,8 +56,7 @@ mp.add_hook("on_load", 9, function()
 
         if list[domain] then
             msg.info("Domain match found: " .. domain)
-            mp.set_property("file-local-options/ytdl-format", ytdlCustom)
-            msg.info("Changed ytdl-format to: " .. mp.get_property("ytdl-format"))
+            update_ytdl_format()
         end
     end
 end)
